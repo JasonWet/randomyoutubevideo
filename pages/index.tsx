884d36b39d1b0289244a67a1d557c6c4e449857e
JasonWet <jason@colonialpress.net>
2021-01-18 17:03:19 -0500

    added YouTube API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import Head from 'next/head';
-import Button from '../components/ui/Button'
 import styled from "styled-components";
-export default function Home() {
+
+const YOUTUBE_VIDEO_API = 'https://www.googleapis.com/youtube/v3/search';
+
+export async function getServerSideProps() {
+  const res = await fetch(`${YOUTUBE_VIDEO_API}?kind=puppet&key=${process.env.YOUTUBE_API_KEY}`)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+
+export default function Home({ data }) {
+  console.info(data);
   <Head>
       <title>randomYouTube.video</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
       <meta name="description" content="Random YouTube Video"></meta>
   </Head>
   return (
