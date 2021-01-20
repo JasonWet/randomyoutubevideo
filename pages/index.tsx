d7aea7bebeb54d119d918ae92249ecf721ae285f
JasonWet <jason@colonialpress.net>
2021-01-19 20:49:02 -0500

    working stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,40 @@
 import Head from 'next/head';
 import styled from "styled-components";
+import ReactPlayer from 'react-player';
 
 const YOUTUBE_SEARCH_API = 'https://www.googleapis.com/youtube/v3/search';
-const YOUTUBE_VIDEO_API = 'https://www.googleapis.com/youtube/v3/video';
+const YOUTUBE_VIDEO_API = 'https://www.googleapis.com/youtube/v3/videos';
 export async function getServerSideProps() {
   let choice = Math.floor(Math.random() * 50) + 1;
 
+  // Get List of YouTube Videos
   let videoId = Math.random().toString(36).substr(2, 3);
   const res = await fetch(`${YOUTUBE_SEARCH_API}?part=snippet&relatedToVideoID=${videoId}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`)
   let data = await res.json()
-  const nextRes = await fetch(`${YOUTUBE_SEARCH_API}?part=snippet&relatedToVideoID=${videoId}&maxResults=50&pageToken=${data.nextPageToken}&key=${process.env.YOUTUBE_API_KEY}`)
-  let nextPage = await nextRes.json()
-  
+
   return {
     props: {
       data,
-      choice,
-      nextPage
+      choice
     }
   }
 }
 
 
 export default function Home(props) {
-  console.log("data", props.data)
-  console.log("nextPage", props.nextPage)
+  console.log('choice', props.choice);
+  console.log('data', props.data);
 
-  let video = props.data.items[props.choice];
-
-  console.log("video", video);
-  console.log("Next page", props.data.nextPageToken);
   <Head>
-      <title>randomYouTube.video</title>
+      <title>randomYouTubevideo</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
       <meta name="description" content="Random YouTube Video"></meta>
   </Head>
   return (
     <Wrapper>
-      
+      <ReactPlayer
+        url={ `"https://www.youtube.com/watch?v=${props.data.items[props.choice].id.videoId}` }
+      />
     </Wrapper>
   )
 }
